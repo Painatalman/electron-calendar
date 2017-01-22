d053b94727ed8b0288095b533c4f9a87b6b71c2b
Painatalman <maiquedi@gmail.com>
2017-01-22 22:56:58 +0000

    update menu, clean up code

--- main.js
+++ main.js
@@ -11,45 +11,63 @@ const template = [
     label: 'File',
     submenu: [
       {
+        label: 'Sync Events',
+        accelerator: 'CmdOrCtrl+s',
+        click() {
+          mainWindow.send(ACTIONS.SYNC_EVENTS);
+        }
+      },
+      {
         label: 'Add Event',
         accelerator: 'CmdOrCtrl+a',
         click (item, focusedWindow) {
-          addEventWindow = new BrowserWindow({
-            backgroundColor: 'transparent',
-            title: 'New Event'
-          });
+          mainWindow.send('add-event');
+          // addEventWindow = new BrowserWindow({
+          //   backgroundColor: 'transparent',
+          //   title: 'New Event'
+          // });
 
-          addEventWindow.loadURL(`file://${__dirname}/add_event_form.html`);
-          addEventWindow.webContents.openDevTools();
+          // addEventWindow.loadURL(`file://${__dirname}/add_event_form.html`);
+          // addEventWindow.webContents.openDevTools();
         }
       },
     ]
   },
 ]
 
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
 const menu = Menu.buildFromTemplate(template)
-Menu.setApplicationMenu(menu)
 
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    backgroundColor: 'transparent',
     title: 'Calendar',
-    show: false
+    show: false,
+    transparent: true,
+    frame: true
   });
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
-
+  Menu.setApplicationMenu(menu)
+  
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow.on(ACTIONS.SIGNAL_SYNCED_EVENTS, () => {
+      app.quit();
+      mainWindow = null;
+    });
+
+    mainWindow.send(ACTIONS.SYNC_EVENTS);
   });
 
   mainWindow.on('ready-to-show', function() {
@@ -58,24 +76,10 @@ function createWindow () {
    });
 }
 
-ipcMain.on('add-event', (event, data) => {
-  console.log(data);
-  mainWindow.send('add-event', data);
-});
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on('ready', createWindow);
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
@@ -85,5 +89,3 @@ app.on('activate', function () {
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,30 +1,34 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-let Calendar = require('@painatalman/eventcalendarjs');
 const fs = require('fs');
 
+let Calendar = require('@painatalman/eventcalendarjs');
+
 const EVENTS_FILE = './_data/events.json';
+const ACTIONS = require('./actions.js');
 
 // TEST
-let eventSettingObjects = require(EVENTS_FILE);
+let eventObjects = require(EVENTS_FILE);
 
-if (!(eventSettingObjects instanceof Array)) {
-  eventSettingObjects = [];
+if (!(eventObjects instanceof Array)) {
+  eventObjects = [];
 }
 
-let calendar = new Calendar("#app", {pictureUrl: 'https://lh3.googleusercontent.com/A798ePpnjRpz5TUnrmxMYOddTo9p172jh1S03cwaEWwV3QSz-rNveC-D2G4qlGhKFXTP2_sTdWdlWkc7CdvAWFHnPAkyJUcdJ9bQvYzsxkGym3VPMiEpaH4jPVsQDzsRHsPESW8bD6-0SVExtl2TfKtekE789WELSdD31XrNgYX4p-jrft4RIkrx-zeUHogDFKb3OYOXEUxWb8rAUmU6u4mNi5JV1Te9_muN2p-LcNmnO1LH7Anr_vYcjuyr8KKPeEaNvik7MQthIPhy7idj0iXA4y4AATLSToZvCztuMedFmoui5HyTL141SvqLWA5cw9i8v74t5oYPQNCgcNNVpmNQ-4mS0gzGnm0-3sn6SkmW2_tT-QSbv5gK7S9iv9GWfh0UKquymwrQYmxeQwf4_J-aTtaItYyqcvZpBr5xQ1vM2HyLFFEmgeJVJZn3RjAWqhiCeVo8zyUEBFt2cO_B7hUouPnSWJN2Qfvgmi8Idh50-odWXaUdiHkWaU9T49BP1Jnm-zRbUZVtLij4ZnXYijYW3bdRZi5BI7TZOR88vZt0nPMUnVXV19YEu3XCAmI9hbZt=w1010-h606-no', eventSettingObjects: eventSettingObjects});
+let calendar = new Calendar("#app", {pictureUrl: 'https://lh3.googleusercontent.com/A798ePpnjRpz5TUnrmxMYOddTo9p172jh1S03cwaEWwV3QSz-rNveC-D2G4qlGhKFXTP2_sTdWdlWkc7CdvAWFHnPAkyJUcdJ9bQvYzsxkGym3VPMiEpaH4jPVsQDzsRHsPESW8bD6-0SVExtl2TfKtekE789WELSdD31XrNgYX4p-jrft4RIkrx-zeUHogDFKb3OYOXEUxWb8rAUmU6u4mNi5JV1Te9_muN2p-LcNmnO1LH7Anr_vYcjuyr8KKPeEaNvik7MQthIPhy7idj0iXA4y4AATLSToZvCztuMedFmoui5HyTL141SvqLWA5cw9i8v74t5oYPQNCgcNNVpmNQ-4mS0gzGnm0-3sn6SkmW2_tT-QSbv5gK7S9iv9GWfh0UKquymwrQYmxeQwf4_J-aTtaItYyqcvZpBr5xQ1vM2HyLFFEmgeJVJZn3RjAWqhiCeVo8zyUEBFt2cO_B7hUouPnSWJN2Qfvgmi8Idh50-odWXaUdiHkWaU9T49BP1Jnm-zRbUZVtLij4ZnXYijYW3bdRZi5BI7TZOR88vZt0nPMUnVXV19YEu3XCAmI9hbZt=w1010-h606-no', eventSettingObjects: eventObjects});
 
 let {ipcRenderer} = require('electron');
 
-ipcRenderer.on('add-event', (event, data) => {
-  // console.log('trying to create new event instance with ', data);
-  // console.log('if successful, it must be added to the json file, too', data);
-  calendar.createAndAddEvent(data,{
-    success: () => {
-      fs.writeFile(EVENTS_FILE, JSON.stringify(calendar.getEvents(true)) , 'utf-8');
-    }
-  });
-  // ipcRenderer.send('save-event', data);
+function syncEvents() {
+  fs.writeFile(EVENTS_FILE, JSON.stringify(calendar.getEvents(true)) , 'utf-8');
 
+  ipcRenderer.send(ACTIONS.SIGNAL_SYNCED_EVENTS);
+}
+
+ipcRenderer.on(ACTIONS.ADD_EVENT, (event, data) => {
+  calendar.showAddEventForm();
+});
+
+ipcRenderer.on(ACTIONS.SYNC_EVENTS, () => {
+  syncEvents();
 });
